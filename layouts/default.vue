b92d536b1557d49102d04df6f331ab01bb5db0ee
Louis Place <l.place@lehibou.com>
2024-08-01 21:49:59 +0400

    feat: mobile version

---
+++ layouts/default.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <nav>
+      <ul class="flex flex-row justify-center items-center">
+        <li class="z-10">
+          <h1 class="pt-3">
+            Portfolio
+          </h1>
+        </li>
+        <ul class="hidden flex-row">
+          <li>About me</li>
+          <li>Experiences</li>
+          <li>Educations</li>
+          <li>projects</li>
+        </ul>
+      </ul>
+      <a
+        class="absolute top-0 left-0 p-3 mt-2 z-10 hover:cursor-pointer"
+        @click="handleBurgerMenu"
+      >
+        <Icon :name="menuOpened ? 'material-symbols:close-rounded' : 'material-symbols:menu-rounded'" />
+      </a>
+      <div
+        v-if="menuOpened"
+        class="menu-item absolute top-0 left-0 bg-sand w-screen h-screen flex flex-col justify-center items-center gap-10"
+      >
+        <NuxtLink><h1>About me</h1></NuxtLink>
+        <NuxtLink><h1>Experiences</h1></NuxtLink>
+        <NuxtLink><h1>Educations</h1></NuxtLink>
+        <NuxtLink><h1>Projects</h1></NuxtLink>
+      </div>
+    </nav>
+    <div class="border-red-800 border-3">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const menuOpened = ref<boolean>(false)
+
+function handleBurgerMenu() {
+  menuOpened.value = !menuOpened.value
+}
+</script>
+
+<style scoped lang="postcss">
+li {
+  @apply flex px-10 items-center;
+}
+
+h1 {
+  @apply text-3xl;
+
+}
+.menu-item {
+  h1 {
+    @apply hover:cursor-pointer duration-100 hover:text-burnedSand select-none;
+  }
+}
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './components/**/*.{js,vue,ts}',
+    './layouts/**/*.vue',
+    './pages/**/*.vue',
+    './plugins/**/*.{js,ts}',
+    './app.vue',
+    './error.vue',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['"Times New Roman"', ...defaultTheme.fontFamily.sans],
+      },
+    },
+    colors: {
+      sand: '#F9D6BA',
+      burnedSand: '#FF4D00',
+    },
+  },
+  plugins: [],
+}
