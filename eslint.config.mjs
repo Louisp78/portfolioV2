9f06f717a90869267458db5709d459e3f8e5a4e0
Louis Place <louis.place@epita.fr>
2024-08-21 19:18:17 +0200

    chore: better eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,5 +8,23 @@ export default withNuxt({
       order: ['script', 'template', 'style'],
     }],
     'vue/no-empty-component-block': 'error',
+    'vue/max-attributes-per-line': [
+      'error',
+      {
+        singleline: {
+          max: 1,
+        }, // Forces single-line tags to have only one attribute per line
+        multiline: {
+          max: 1, // Forces multiline tags to have only one attribute per line
+        },
+      },
+    ],
+    'vue/html-closing-bracket-newline': [
+      'error',
+      {
+        singleline: 'never',
+        multiline: 'always',
+      },
+    ],
   },
 })
