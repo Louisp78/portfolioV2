9463d3c5ca8d241187278438c33399185b42a5cc
Louis Place <louis.place@epita.fr>
2024-12-16 14:29:55 +0100

    refactor(index): delete dead code

--- pages/index.vue
+++ pages/index.vue
@@ -511,14 +511,4 @@ const allSkillList = computed(() => {
 ul {
     @apply flex flex-row gap-3;
 }
-
-.fade-enter-active, .fade-leave-active {
-  transition: max-height opacity 0.5s ease-in-out;
-}
-.fade-enter-from, .fade-leave-to {
-  opacity: 0;
-}
-.fade-enter-to, .fade-leave-from {
-  opacity: 1;
-}
 </style>
